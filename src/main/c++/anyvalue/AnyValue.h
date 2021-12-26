037857b974181c5dd523cbde1c9eb13173840ec8
Walter Van Herck <walter.vanherck@gmail.com>
2021-12-26 11:15:40 +0000

    Add source file structure for AnyValue

---
+++ src/main/c++/anyvalue/AnyValue.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - Sequencer
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2021 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+/**
+ * @file AnyValue.h
+ * @brief Header file for AnyValue.
+ * @date 26/12/2021
+ * @author Walter Van Herck (IO)
+ * @copyright 2010-2021 ITER Organization
+ * @details This header file contains the definition of the AnyValue class.
+ */
+
+#ifndef _SUP_AnyValue_h_
+#define _SUP_AnyValue_h_
+
+namespace sup
+{
+namespace dto
+{
+/**
+ * @brief Class for structured data transfer objects.
+ */
+class AnyValue
+{
+};
+
+}  // namespace dto
+
+}  // namespace sup
+
+#endif  // _SUP_AnyValue_h_
