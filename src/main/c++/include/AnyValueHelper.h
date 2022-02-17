b6673d9f034bce17d8925ebfd2094271e6c7339a
Walter Van Herck <walter.vanherck@iter.org>
2022-02-17 16:20:29 +0000

    Add AnyValue serialize function

---
+++ src/main/c++/include/AnyValueHelper.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+/**
+ * @file AnyValueHelper.h
+ * @brief Header file for AnyValue helper classes and functions.
+ * @date 17/02/2022
+ * @author Walter Van Herck (IO)
+ * @copyright 2010-2022 ITER Organization
+ * @details This header file contains the definition of the AnyValue helper classes and functions.
+ */
+
+#ifndef _SUP_AnyValueHelper_h_
+#define _SUP_AnyValueHelper_h_
+
+#include "AnyValue.h"
+#include "IAnySerializer.h"
+
+namespace sup
+{
+namespace dto
+{
+
+void SerializeAnyValue(const AnyValue& anyvalue, IAnySerializer<AnyValue>& serializer);
+
+}  // namespace dto
+
+}  // namespace sup
+
+#endif  // _SUP_AnyValueHelper_h_
