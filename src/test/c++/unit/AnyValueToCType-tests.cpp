40dc7455927d4e71833ddd411003e3697b044653
Walter Van Herck <walter.vanherck@iter.org>
2022-02-21 10:40:07 +0000

    Add empty and array C-type cast unit tests

--- src/test/c++/unit/AnyValueToCType-tests.cpp
+++ src/test/c++/unit/AnyValueToCType-tests.cpp
@@ -25,28 +25,22 @@
 
 using namespace sup::dto;
 
-struct __attribute__((__packed__)) TwoScalarsType
-{
-  int8 signed_scalar;
-  uint8 unsigned_scalar;
-};
-
 struct __attribute__((__packed__)) AddressType
 {
   char street[STRING_MAX_LENGTH];
   uint16 number;
 };
 
-TEST(AnyValueToCTypeTest, Success)
+struct __attribute__((__packed__)) TwoScalarsType
 {
-  AnyValue two_scalars = {{
-    {"signed", {SignedInteger8, 1}},
-    {"unsigned", {UnsignedInteger8, 12}}
-  }};
-  auto two_scalars_c = two_scalars.As<TwoScalarsType>();
-  EXPECT_EQ(two_scalars_c.signed_scalar, 1);
-  EXPECT_EQ(two_scalars_c.unsigned_scalar, 12);
-}
+  int8 signed_scalar;
+  uint8 unsigned_scalar;
+};
+
+struct __attribute__((__packed__)) ArrayType
+{
+  uint32 elements[5];
+};
 
 TEST(AnyValueToCTypeTest, StringFields)
 {
@@ -66,4 +60,38 @@ TEST(AnyValueToCTypeTest, StringFields)
   // Fields are unchanged
   EXPECT_STREQ(address_c.street, "Main street");
   EXPECT_EQ(address_c.number, 1033);
+}
+
+TEST(AnyValueToCTypeTest, Empty)
+{
+  // Empty AnyValue has empty byte array representation
+  AnyValue empty{};
+  auto byte_array = AnyValueToByteArray(empty);
+  EXPECT_EQ(byte_array.size(), 0);
+}
+
+TEST(AnyValueToCTypeTest, Struct)
+{
+  AnyValue two_scalars = {{
+    {"signed", {SignedInteger8, 1}},
+    {"unsigned", {UnsignedInteger8, 12}}
+  }};
+  auto two_scalars_c = two_scalars.As<TwoScalarsType>();
+  EXPECT_EQ(two_scalars_c.signed_scalar, 1);
+  EXPECT_EQ(two_scalars_c.unsigned_scalar, 12);
+}
+
+TEST(AnyValueToCTypeTest, Array)
+{
+  // Empty AnyValue has empty byte array representation
+  AnyValue my_array(5, UnsignedInteger32, "my_uint32_array");
+  for (unsigned i=0; i<5; ++i)
+  {
+    my_array[i] = 10 * i;
+  }
+  auto my_array_c = my_array.As<ArrayType>();
+  for (unsigned i=0; i<5; ++i)
+  {
+    EXPECT_EQ(my_array_c.elements[i], 10 * i);
+  }
 }
