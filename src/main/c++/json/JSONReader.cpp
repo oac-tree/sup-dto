7b5385c4637d5e0788196a24a99d15741f6029c9
Walter Van Herck <walter.vanherck@gmail.com>
2022-03-01 19:14:59 +0000

    Add JSONReader

---
+++ src/main/c++/json/JSONReader.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "JSONReader.h"
+
+#include "AnyType.h"
+#include "AnyTypeBuilder.h"
+
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/reader.h"
+
+namespace sup
+{
+namespace dto
+{
+
+AnyType JSONParseAnyType(std::istream& json_stream)
+{
+  AnyTypeBuilder builder;
+  rapidjson::IStreamWrapper istream(json_stream);
+  rapidjson::Reader reader;
+
+  reader.Parse(istream, builder);
+  return builder.MoveAnyType();
+}
+
+}  // namespace dto
+
+}  // namespace sup
---
+++ src/main/c++/json/JSONReader.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+/**
+ * @file JSONReader.h
+ * @brief Header file for the JSONReader classes.
+ * @date 01/03/2022
+ * @author Walter Van Herck (IO)
+ * @copyright 2010-2022 ITER Organization
+ * @details This header file contains the definition of the JSONReader classes.
+ */
+
+#ifndef _SUP_JSONReader_h_
+#define _SUP_JSONReader_h_
+
+#include <istream>
+
+namespace sup
+{
+namespace dto
+{
+class AnyType;
+class AnyValue;
+
+AnyType JSONParseAnyType(std::istream& json_str);
+
+}  // namespace dto
+
+}  // namespace sup
+
+#endif  // _SUP_JSONReader_h_
