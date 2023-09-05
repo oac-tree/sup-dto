23fe473ccb0283d5fa4796cccdaebf5fc7737182
Walter Van Herck <walter.vanherck@iter.org>
2023-09-05 12:43:21 +0000

    Add BinaryValueParser

---
+++ src/lib/sup/dto/parse/binary_value_parser.cpp
@@ -0,0 +1,125 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Unit test code
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "binary_value_parser.h"
+
+#include <sup/dto/parse/binary_parser_functions.h>
+
+#include <sup/dto/anyvalue.h>
+#include <sup/dto/anyvalue_helper.h>
+
+#include <functional>
+#include <map>
+
+namespace
+{
+using namespace sup::dto;
+template <typename T>
+void AssignBinaryScalarT(AnyValue& anyvalue, ByteIterator& it, const ByteIterator& end)
+{
+  anyvalue = ParseBinaryScalarT<T>(it, end);
+}
+
+void AssignBinaryString(AnyValue& anyvalue, ByteIterator& it, const ByteIterator& end)
+{
+  anyvalue = ParseBinaryString(it, end);
+}
+}  // unnamed namespace
+
+namespace sup
+{
+namespace dto
+{
+
+BinaryValueParser::BinaryValueParser(ByteIterator& it, const ByteIterator& end)
+  : m_it{it}
+  , m_end{end}
+{}
+
+BinaryValueParser::~BinaryValueParser() = default;
+
+bool BinaryValueParser::IsFinished() const
+{
+  return m_it == m_end;
+}
+
+void BinaryValueParser::EmptyProlog(AnyValue*)
+{}
+
+void BinaryValueParser::EmptyEpilog(AnyValue*)
+{}
+
+void BinaryValueParser::StructProlog(AnyValue*)
+{}
+
+void BinaryValueParser::StructMemberSeparator()
+{}
+
+void BinaryValueParser::StructEpilog(AnyValue*)
+{}
+
+void BinaryValueParser::MemberProlog(AnyValue*, const std::string&)
+{}
+
+void BinaryValueParser::MemberEpilog(AnyValue*, const std::string&)
+{}
+
+void BinaryValueParser::ArrayProlog(AnyValue*)
+{}
+
+void BinaryValueParser::ArrayElementSeparator()
+{}
+
+void BinaryValueParser::ArrayEpilog(AnyValue*)
+{}
+
+void BinaryValueParser::ScalarProlog(AnyValue* anyvalue)
+{
+  using ScalarParserFunction = std::function<void(AnyValue&, ByteIterator&, const ByteIterator&)>;
+  static std::map<TypeCode, ScalarParserFunction> assign_map {
+    {TypeCode::Bool, AssignBinaryScalarT<boolean> },
+    {TypeCode::Char8, AssignBinaryScalarT<char8> },
+    {TypeCode::Int8, AssignBinaryScalarT<int8> },
+    {TypeCode::UInt8, AssignBinaryScalarT<uint8> },
+    {TypeCode::Int16, AssignBinaryScalarT<int16> },
+    {TypeCode::UInt16, AssignBinaryScalarT<uint16> },
+    {TypeCode::Int32, AssignBinaryScalarT<int32> },
+    {TypeCode::UInt32, AssignBinaryScalarT<uint32> },
+    {TypeCode::Int64, AssignBinaryScalarT<int64> },
+    {TypeCode::UInt64, AssignBinaryScalarT<uint64> },
+    {TypeCode::Float32, AssignBinaryScalarT<float32> },
+    {TypeCode::Float64, AssignBinaryScalarT<float64> },
+    {TypeCode::String, AssignBinaryString }
+  };
+  auto it = assign_map.find(anyvalue->GetTypeCode());
+  if (it == assign_map.end())
+  {
+    throw ParseException("BinaryValueParser: unknown scalar type code");
+  }
+  it->second(*anyvalue, m_it, m_end);
+}
+
+void BinaryValueParser::ScalarEpilog(AnyValue*)
+{}
+
+}  // namespace dto
+
+}  // namespace sup
---
+++ src/lib/sup/dto/parse/binary_value_parser.h
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_DTO_BINARY_VALUE_PARSER_H_
+#define SUP_DTO_BINARY_VALUE_PARSER_H_
+
+#include <sup/dto/parse/binary_parser.h>
+
+#include <sup/dto/basic_scalar_types.h>
+#include <sup/dto/i_any_visitor.h>
+
+namespace sup
+{
+namespace dto
+{
+class AnyValue;
+
+class BinaryValueParser : public IAnyVisitor<AnyValue>
+{
+public:
+  BinaryValueParser(ByteIterator& it, const ByteIterator& end);
+  ~BinaryValueParser();
+
+  BinaryValueParser(const BinaryValueParser& other) = delete;
+  BinaryValueParser(BinaryValueParser&& other) = delete;
+  BinaryValueParser& operator=(const BinaryValueParser& other) = delete;
+  BinaryValueParser& operator=(BinaryValueParser&& other) = delete;
+
+  bool IsFinished() const;
+
+  void EmptyProlog(AnyValue* anyvalue) override;
+  void EmptyEpilog(AnyValue* anyvalue) override;
+
+  void StructProlog(AnyValue* anyvalue) override;
+  void StructMemberSeparator() override;
+  void StructEpilog(AnyValue* anyvalue) override;
+
+  void MemberProlog(AnyValue* anyvalue, const std::string& member_name) override;
+  void MemberEpilog(AnyValue* anyvalue, const std::string& member_name) override;
+
+  void ArrayProlog(AnyValue* anyvalue) override;
+  void ArrayElementSeparator() override;
+  void ArrayEpilog(AnyValue* anyvalue) override;
+
+  void ScalarProlog(AnyValue* anyvalue) override;
+  void ScalarEpilog(AnyValue* anyvalue) override;
+  bool HandleToken();
+
+private:
+  ByteIterator& m_it;
+  const ByteIterator& m_end;
+};
+
+}  // namespace dto
+
+}  // namespace sup
+
+#endif  // SUP_DTO_BINARY_VALUE_PARSER_H_
