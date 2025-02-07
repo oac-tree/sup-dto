426ffa976dc3282c172d4e2421c5464a3ea5922d
Walter Van Herck <walter.vanherck@iter.org>
2025-02-07 16:16:28 +0000

    Add AnyValueConvertNode

---
+++ src/lib/sup/dto/anyvalue/anyvalue_convert_node.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "anyvalue_convert_node.h"
+
+namespace sup
+{
+namespace dto
+{
+
+AnyValueConvertNode::AnyValueConvertNode(AnyValue* left, const AnyValue* right,
+                                         std::size_t n_children)
+  : m_left{left}
+  , m_right{right}
+  , m_n_children{n_children}
+  , m_index{0}
+{}
+
+}  // namespace dto
+
+}  // namespace sup
---
+++ src/lib/sup/dto/anyvalue/anyvalue_convert_node.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - DTO
+ *
+ * Description   : Data transfer objects for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_DTO_ANYVALUE_CONVERT_NODE_H_
+#define SUP_DTO_ANYVALUE_CONVERT_NODE_H_
+
+#include <sup/dto/anyvalue.h>
+
+namespace sup
+{
+namespace dto
+{
+struct AnyValueConvertNode
+{
+  AnyValueConvertNode(AnyValue* left, const AnyValue* right, std::size_t n_children);
+  ~AnyValueConvertNode() = default;
+
+  AnyValueConvertNode(const AnyValueConvertNode&) = delete;
+  AnyValueConvertNode& operator=(const AnyValueConvertNode&) = delete;
+  AnyValueConvertNode(AnyValueConvertNode&&) = default;
+  AnyValueConvertNode& operator=(AnyValueConvertNode&&) = default;
+
+  AnyValue* m_left;
+  const AnyValue* m_right;
+  std::size_t m_n_children;
+  std::size_t m_index;
+};
+
+}  // namespace dto
+
+}  // namespace sup
+
+#endif  // SUP_DTO_ANYVALUE_CONVERT_NODE_H_
